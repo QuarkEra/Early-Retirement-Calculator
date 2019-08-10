9d01f74dd758a1665fd1a4791750b215007d542f
QuarkEra <josephseaton@physics.org>
2019-08-10 14:09:33 +0100

    Added index and js script files
    nyooooooom

---
+++ app.js
@@ -0,0 +1,72 @@
+// Listen for form submit event
+document.getElementById('retirement-form').addEventListener('submit', calculateResults);
+
+// Calculating the results
+function calculateResults(e) {
+    // UI Vars
+    const UIcurrentPension = document.getElementById('current-pension');
+    const UIcurrentMonthly = document.getElementById('current-monthly');
+    const UIvoluntaryMonthly = document.getElementById('voluntary-monthly');
+    const UImonthlyOutgoings = document.getElementById('monthly-outgoings');
+    const UIyearsToRetire = document.getElementById('years-to-retire');
+
+    // Results vars
+    const annualOutgoings = parseFloat(UImonthlyOutgoings.value) * 12;
+    const annualRetirementContributions = (parseFloat(UIcurrentMonthly.value) + parseFloat(UIvoluntaryMonthly.value)) * 12;
+    let retirementFund = parseFloat(UIcurrentPension.value) + annualRetirementContributions;
+    console.log('annual outgoings: ' + annualOutgoings);
+    console.log('annual contribs: ' + annualRetirementContributions);
+    console.log('retirement fund: ' + retirementFund);
+    var yearsToRetire = 0;
+    var i = 0;
+
+    // (annualOutgoings/0.04) < retirementFund
+    while (i<50) {
+        retirementFund += annualRetirementContributions;
+        console.log('WHILE retirement fund: ' + retirementFund.toFixed(2));
+        yearsToRetire++;
+        i++;
+        // interest calculated at 2% return annually
+        retirementFund += (retirementFund * 0.02); 
+        if ((annualOutgoings/0.04) < retirementFund) {
+            break
+        }
+    }
+
+    if ((annualOutgoings/0.04) < retirementFund) {
+        UIyearsToRetire.value = yearsToRetire;
+        console.log('If block ran condition met...')
+    } else {
+        showError('Check input accuracy.');
+        console.log('If block ran condition NOT met...')
+    }
+
+    e.preventDefault();
+}
+
+// show error
+function showError(error) {
+    // create a div
+    const errorDiv = document.createElement('div');
+
+    // get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// clear error
+function clearError() {
+    document.querySelector('.alert').remove();
+}
